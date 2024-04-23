a0739aee1af80d51144c3785812923d854cc6426
khoai <khanhphuongdo28@gmail.com>
2024-04-22 21:44:33 -0500

    delete redundant comments

--- main.py
+++ main.py
@@ -206,12 +206,6 @@ class GamePlayState(GameState):
         # Times
         self.remaining_time = 3 * 60 # 3 minutes
         self.start_time = time.time()
-        # self.countdown_font = pygame.font.SysFont(None, 200)  # Choose a large font size for countdown
-        # self.countdown_color = (0, 0, 0)  # Black color for the countdown text
-        # self.countdown_sound = pygame.mixer.Sound('assets/audio/countdown_tick.wav')  # Load countdown tick sound
-        # self.countdown_sound.set_volume(0.5)  # Set volume for countdown tick sound
-        # self.countdown_sound_tick = 1  # Duration of each tick sound in seconds
-        # self.tick_time = time.time()  # Time tracker for tick sound
         
         # Player and Items
         self.player = Player((MID_X, GROUND_Y),          # position
